d91f9220ddc7cc0170dad02c0c9a487188f7227c
Antonio <antonioparis2003@gmail.com>
2024-01-26 16:32:23 +0000

    Update Main Menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,6 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Camera bloodclasscamera;
+    public Camera cleanclasscamera;
+    float time;
+    [SerializeField] bool blood = false;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 3f)
+        {
+            blood = !blood;
+            if (blood)
+            {
+                Debug.Log("aaa");
+                showBloodCam();
+            }
+            if (!blood)
+            {
+                Debug.Log("bbb");
+                showCleanCam();
+            }
+            time = 0f;
+        }
+    }
+
+    public void showBloodCam()
+    {
+        cleanclasscamera.enabled = false;
+        bloodclasscamera.enabled = true;
+    }
+
+    public void showCleanCam()
+    {
+        bloodclasscamera.enabled = false;
+        cleanclasscamera.enabled = true;
+    }
+
    public void PlayGame()
     {
         SceneManager.LoadScene("Antonio");
